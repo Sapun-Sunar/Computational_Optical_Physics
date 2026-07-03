e17d1e3a360ac240ae238838a1634d6157643b1d
shreyanshsunar123-maker <shreyanshsunar123@gmail.com>
2026-07-03 14:45:32 -0500

    Updated repo structure.

--- LIBS_Spectral_Analysis./Fabry_perot Analysis./sip_decoder.py
+++
@@ -1,7 +0,0 @@
-import sif_parser
-import numpy as np
-
-def decoded_matrix(file_path):
-    raw_data, info = sif_parser.np_open(file_path)
-    image_matrix = np.squeeze(raw_data)
-    return image_matrix
